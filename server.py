5fac376a74d3f85f85466e00a65c8322d40265cd
manujose94 <manujose94unsc@gmail.com>
2019-11-01 20:22:10 +0100

    first push

---
+++ server.py
@@ -0,0 +1,12 @@
+# app.py
+
+import http.server
+import socketserver
+
+PORT = 8007
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
